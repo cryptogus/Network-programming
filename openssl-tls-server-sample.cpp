462601e2fd440cde4ebdfa722114f3ff95764268
cryptogus <wnelem1234@naver.com>
2024-01-26 18:32:24 +0900

    change c style to c++ style

--- openssl-tls-server-sample.cpp
+++ openssl-tls-server-sample.cpp
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 // g++ -o tls_server openssl-tls-server-sample.cpp -lssl -lcrypto
@@ -31,7 +29,7 @@ int create_server_socket(int port) {
     server_addr.sin_port = htons(port);
 
     // 소켓과 주소 바인딩
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) == -1) {
         perror("Binding error");
         close(server_socket);
         return -1;
@@ -59,21 +57,21 @@ int main() {
 
     // SSL 컨텍스트 생성
     ctx = SSL_CTX_new(TLS_server_method());
-    if (ctx == NULL) {
-        fprintf(stderr, "SSL_CTX_new error.\n");
+    if (ctx == nullptr) {
+        std::cerr << "SSL_CTX_new error." << std::endl;
         ERR_print_errors_fp(stderr);
         return 1;
     }
 
     // 인증서와 키 로드
     if (SSL_CTX_use_certificate_file(ctx, CERT_FILE, SSL_FILETYPE_PEM) <= 0) {
-        fprintf(stderr, "Error loading certificate from file.\n");
+        std::cerr << "Error loading certificate from file." << std::endl;
         ERR_print_errors_fp(stderr);
         return 1;
     }
 
     if (SSL_CTX_use_PrivateKey_file(ctx, KEY_FILE, SSL_FILETYPE_PEM) <= 0) {
-        fprintf(stderr, "Error loading private key from file.\n");
+        std::cerr << "Error loading private key from file." << std::endl;
         ERR_print_errors_fp(stderr);
         return 1;
     }
@@ -81,12 +79,12 @@ int main() {
     // 서버 소켓 생성 및 연결 대기
     server = create_server_socket(SERVER_PORT);
     if (server < 0) {
-        fprintf(stderr, "Server socket creation error.\n");
+        std::cerr << "Server socket creation error." << std::endl;
         return 1;
     }
 
     // 클라이언트 연결 대기
-    client = accept(server, NULL, NULL);
+    client = accept(server, nullptr, nullptr);
     if (client == -1) {
         perror("Accept error");
         close(server);
@@ -99,7 +97,7 @@ int main() {
 
     // SSL 핸드셰이크 수행
     if (SSL_accept(ssl) == -1) {
-        fprintf(stderr, "SSL_accept error.\n");
+        std::cerr << "SSL_accept error." << std::endl;
         ERR_print_errors_fp(stderr);
         close(client);
         close(server);
@@ -111,7 +109,7 @@ int main() {
     int bytes_received = SSL_read(ssl, buffer, sizeof(buffer));
     if (bytes_received > 0) {
         buffer[bytes_received] = '\0';
-        printf("Received: %s\n", buffer);
+        std::cout << "Received: " << buffer << std::endl;
 
         // 데이터 전송
         const char *msg = "Hello, Client!";
