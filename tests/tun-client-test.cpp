e8ac9aed1a8903c5252ef35e879e6f23d2cafc40
cryptogus <wnelem1234@naver.com>
2024-02-02 22:47:34 +0900

    Change server's public IP address for generality

--- tests/tun-client-test.cpp
+++ tests/tun-client-test.cpp
@@ -3,7 +3,7 @@
 char *SERVER_HOST;
 
 int main(void) {
-    SERVER_HOST = "127.0.0.1";
+    SERVER_HOST = "122.211.166.156"; // server public ip
     Tun_Interface VPN_client(SERVER_HOST, "12345");
     VPN_client.run();
 }
