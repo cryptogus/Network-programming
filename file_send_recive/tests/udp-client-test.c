7efde34f4fe808d942708ebaffb710e54ca34048
cryptogus <wnelem1234@naver.com>
2024-02-23 18:58:20 +0900

    Modify filepath

--- file_send_recive/tests/udp-client-test.c
+++ file_send_recive/tests/udp-client-test.c
@@ -1,7 +1,7 @@
 #include "udp.h"
 
 int main(int argc, char *argv[]) {
-    udp_client("127.0.0.1", "12345", "/home/hhlee/wallpadtest/tests/test.txt");
+    udp_client("127.0.0.1", "12345", "test.txt");
 
     return 0;
 }
